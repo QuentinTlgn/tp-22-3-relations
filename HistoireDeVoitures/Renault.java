cbc0157028931365a036d6c7d828f15212c34312
QuentinTlgn <tauleigq@3il.fr>
2023-11-29 11:14:27 +0100

    Histoire de voiture : Renault

---
+++ HistoireDeVoitures/Renault.java
@@ -0,0 +1,22 @@
+package HistoireDeVoitures;
+
+public class Renault extends Voiture{
+	private int anneeFabrication;
+	private int reductionConstructeur;
+
+	public Renault(int vitesseMax, float prixDeBase, String couleur, int anneeFabrication, int reductionConstructeur) {
+		super(vitesseMax, prixDeBase, couleur);
+		this.anneeFabrication = anneeFabrication;
+		this.reductionConstructeur=reductionConstructeur;
+
+	}
+	
+	public double calculerPrixDeVente() {
+		return super.calculerPrixDeVente() - reductionConstructeur;
+	}
+
+	public int getAnneeFabrication() {
+		return anneeFabrication;
+	}
+	
+}

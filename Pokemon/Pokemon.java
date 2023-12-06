e70916868647c59424f945435ca66ce409850741
QuentinTlgn <tauleigq@3il.fr>
2023-12-06 13:57:44 +0100

    Finis Pokemon.java

--- Pokemon/Pokemon.java
+++ Pokemon/Pokemon.java
@@ -1,5 +1,43 @@
 package Pokemon;
 
 public class Pokemon {
-
+	private String nom;
+	private int pv;
+	private int atk;
+	
+	public Pokemon(String nom, int pv, int atk) {
+		this.nom = nom;
+		this.pv = pv;
+		this.atk = atk;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public int getPv() {
+		return pv;
+	}
+	public int getAtk() {
+		return atk;
+	}
+	
+	public boolean estKO() {
+		return pv==0;
+	}
+	
+	
+	private void prendreDegats(int deg) {
+		this.pv -= deg;
+		if (pv < 0)
+			pv = 0;
+	}
+	public void attaquer(Pokemon p) {
+		p.prendreDegats(atk);
+	}
+	
+	@Override
+	public String toString() {
+		return "[Pokémon "+ nom + ", pv: "+pv;
+	}
+	
 }
